c211b57e14da45ca6c50f83b2bf1d4865c6bb7b1
Kellie Hall <rockerr.hello@gmail.com>
2023-10-26 19:30:10 -0400

    cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import express, { Application } from 'express';
 const app: Application = express();
 
-// routes
 const blogRoutes = require('./blog/blog-routes');
 const healthcheckRoutes = require('./common/healthcheck');
 
 app.use('/blog', blogRoutes);
 app.use('/healthcheck', healthcheckRoutes);
-
-// app.use('/', (req: Request, res: Response): void => {
-//     res.send('Hello world!');
-// });
-
 
 const port = 8080;
 app.listen(port, () => {
