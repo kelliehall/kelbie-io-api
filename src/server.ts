7b28bc719202c73731a93d5951d837ce486cbab9
Kellie Hall <rockerr.hello@gmail.com>
2023-10-26 11:09:57 -0400

    remove cassandra

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,5 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
-const cassandra = require('cassandra-driver');
-// const client = new cassandra.Client({ contactPoints: ['localhost'], localDataCenter: 'datacenter1' });
-
-// const query = 'INSERT INTO keyspace_name.table_name (column1, column2) VALUES (?, ?)';
-// const params = ['value1', 'value2'];
-
-// client.execute(query, params, { prepare: true })
-//   .then(result => {
-//     console.log('Data inserted successfully');
-//   })
-//   .catch(error => {
-//     console.error('Error inserting data:', error);
-//   });
-
 
 // routes
 const blogRoutes = require('./blog/blog-routes');
